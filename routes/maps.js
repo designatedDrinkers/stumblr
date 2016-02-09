3d803ef909a4b0c5a95aa92bf70a723297c38720
Ben Allred <skuttleman@gmail.com>
2016-02-09 11:38:23 -0700

    added /api/maps/route to make list of bar lat/lng strings

--- routes/maps.js
+++ routes/maps.js
@@ -16,4 +16,49 @@ route.get('/', function(request, response, next) {
   }
 });
 
-//
+route.get('/route', function(request, response, next) {
+  var additives = { rankby: 'distance', keyword: 'bar', key: process.env.GOOGLE_API_KEY };
+  var query = combine(request.query, additives);
+  var barcount = query.barcount;
+  delete query.barcount;
+  unirest.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json' + stringify(query))
+  .end(function(data) {
+    var bars = sortBars(data.body.results, barcount);
+    bars = bars.map(latLngBars);
+    bars.unshift(query.location);
+    response.json({ bars: bars, data: data });
+  });
+});
+
+function stringify(query) {
+  var array = Object.keys(query).map(function(key) {
+    return [key, query[key]].join('=');
+  });
+  return '?' + array.join('&');
+}
+
+function combine(obj1, obj2) {
+  var newObject = Object.keys(obj1).reduce(function(obj, key) {
+    obj[key] = obj1[key];
+    return obj;
+  }, {});
+  Object.keys(obj2).reduce(function(obj, key) {
+    obj[key] = obj2[key];
+    return obj;
+  }, newObject);
+  return newObject;
+}
+
+function sortBars(bars, barcount) {
+  var sortedBars = bars.slice(0, barcount);
+  var first = [], last = [];
+  sortedBars.forEach(function(bar, i) {
+    if (i % 2) last.unshift(bar);
+    else first.push(bar);
+  });
+  return first.concat(last);
+}
+
+function latLngBars(bar) {
+  return [bar.geometry.location.lat, bar.geometry.location.lng].join(',');
+}
