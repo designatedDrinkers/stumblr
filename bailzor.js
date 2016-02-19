86d60651d03c1bbca0fa29582e2897bffb661c28
Ben Allred <skuttleman@gmail.com>
2016-02-18 17:06:31 -0700

    fixed bailzor.js & browserified

--- bailzor.js
+++ bailzor.js
@@ -1,8 +1,13 @@
 var mongo = require('./services/db');
 
 mongo.connect().then(function(db) {
-  db.collection('users').findOne({ twitter_name: elana }, function(err, data) {
-    if (err) return console.log(err);
+  db.collection('users').findOne({ twitter_name: 'Elana Kopelevich' }, function(err, data) {
+    if (err) return console.error(err);
     data.routes = data.routes.slice(0, 2);
+    db.collection('users').update({ twitter_name: 'Elana Kopelevich' }, { $set: { routes: data.routes }}, function(err, response) {
+      if (err) return console.error(err);
+      console.log('success');
+      db.close();
+    });
   });
-});
+}).catch(console.error);
